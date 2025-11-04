196759cf159edc2a8561f57001529b40bbb52a41
Eric Hamilton <ericjean873@gmail.com>
2025-11-04 11:46:45 -0800

    2015 day 4

--- 2015/day-4.py
+++ 2015/day-4.py
@@ -17,7 +17,16 @@ def part_one(input: str) -> int:
 
 
 def part_two(input: str) -> int:
-    return 0
+    i = 1
+    while True:
+        key = f"{input}{i}"
+        hash = hashlib.md5(key.encode())
+        hash_hex = hash.hexdigest()
+        if hash_hex.startswith("000000"):
+            break
+        else:
+            i += 1
+    return i
 
 
 asyncio.run(main(2015, 4, part_one, part_two))
