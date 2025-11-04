3e072fdf93e71fa7199dd049d1add661359a80b7
Eric Hamilton <ericjean873@gmail.com>
2025-11-03 16:04:59 -0800

    day one 2015

--- 2015/one.py
+++ 2015/one.py
@@ -2,13 +2,35 @@ import asyncio
 from util import get_input
 
 
-def part_one() -> int:
-    return 0
+def part_one(input: str) -> int:
+    floor = 0
+    for c in input:
+        if c == "(":
+            floor += 1
+        elif c == ")":
+            floor -= 1
+    return floor
+
+
+def part_two(input: str) -> int:
+    floor = 0
+    i = 1
+    for c in input:
+        if c == "(":
+            floor += 1
+        elif c == ")":
+            floor -= 1
+        if floor == -1:
+            return i
+        else:
+            i += 1
+    return -1
 
 
 async def main():
     input = await get_input(2015, 1)
-    print(input)
+    print(f"Part One: {part_one(input)}")
+    print(f"Part Two: {part_two(input)}")
 
 
 asyncio.run(main())
