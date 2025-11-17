275d4040cd4f53c6492493cd33f2e6256d4719bc
Eric Hamilton <ericjean873@gmail.com>
2025-11-17 15:40:16 -0800

    2019 day 19; part two technically produces a correct solution sometimes (???)

---
+++ 2015/day-19.py
@@ -0,0 +1,66 @@
+import asyncio
+import re
+
+import heapdict
+
+from util import main
+
+
+def parse_input(input: str) -> tuple[dict[str, str], str]:
+    r_strs, medicine = input.split("\n\n")
+    replacements: dict[str, str] = {}
+    for line in r_strs.splitlines():
+        value, key = line.split(" => ")
+        replacements[key] = value
+    return replacements, medicine
+
+
+def part_one(input: str) -> int:
+    replacements, medicine = parse_input(input)
+    molecules: set[str] = set()
+    for k in replacements.keys():
+        for m in re.finditer(rf"({replacements[k]})", medicine):
+            start, end = m.span()
+            repl = f"{medicine[:start]}{k}{medicine[end:]}"
+            molecules.add(repl)
+    return len(molecules)
+
+
+# ill be totally honest. this is a modified bfs, but i have zero clue
+# why this works.
+def trace(replacements: dict[str, str], root: str, goal="e") -> int:
+    def molecule_len(molecule: str) -> int:
+        return len(re.findall(r"[A-Z][a-z]?", molecule))
+
+    queue = heapdict.heapdict()
+    queue[(root, 0)] = molecule_len(root)
+    curr_depth = -1
+    visited: set[str] = set()
+    visited.add(root)
+    while len(queue) > 0:
+        (curr, depth), c_len = queue.popitem()
+        if depth > curr_depth:
+            curr_depth = depth
+            print(f"Depth: {curr_depth}, Visited: {len(visited)}")
+        neighbours: set[str] = set()
+        for k in replacements.keys():
+            for m in re.finditer(rf"({k})", curr):
+                start, end = m.span()
+                repl = f"{curr[:start]}{replacements[k]}{curr[end:]}"
+                neighbours.add(repl)
+        for n in neighbours:
+            if n == goal:
+                return depth + 1
+            n_len = molecule_len(n)
+            if n not in visited:
+                visited.add(n)
+                queue[(n, depth + 1)] = n_len
+    return 0
+
+
+def part_two(input: str) -> int:
+    replacements, medicine = parse_input(input)
+    return trace(replacements, medicine)
+
+
+asyncio.run(main(2015, 19, part_one, part_two))
