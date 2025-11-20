7baa03a8f1cc0fe5fea070d2776e385dc35415f9
Eric Hamilton <ericjean873@gmail.com>
2025-11-20 11:46:24 -0800

    day 4 solved

--- 2024/src/solutions/day4.rs
+++ 2024/src/solutions/day4.rs
@@ -1,42 +1,10 @@
 pub struct Solver(pub String);
 
-type Point = (usize, usize);
-
-fn scan_block(block: Vec<&[char]>) -> Vec<(Point, Point)> {
-    for r in block.as_slice() {
-        println!("{:?}", r);
-    }
-    println!();
-    let mut substrings: Vec<(Point, Point, String)> = vec![];
-
-    let diag_one = [block[0][0], block[1][1], block[2][2], block[3][3]];
-    let diag_two = [block[0][3], block[1][2], block[2][1], block[3][0]];
-    substrings.push(((0, 0), (3, 3), diag_one.iter().collect()));
-    substrings.push(((0, 3), (3, 0), diag_two.iter().collect()));
-
-    for i in 0..block.len() {
-        substrings.push(((i, 0), (i, 3), block[i].iter().collect()));
-        let col = [block[0][i], block[1][i], block[2][i], block[3][i]];
-        substrings.push(((0, i), (3, i), col.iter().collect()));
-    }
-    let mut ranges = vec![];
-    for sub in substrings.clone() {
-        match sub.2.as_str() {
-            "XMAS" | "SAMX" => ranges.push((sub.0, sub.1)),
-            _ => (),
-        }
-    }
-    return ranges;
-}
-
-impl super::lib::Puzzle<usize> for Solver {
-    async fn part_one(&self) -> usize {
+impl Solver {
+    fn parse_input(&self) -> Vec<Vec<char>> {
         let mut word_search = vec![vec![]];
         let mut row = 0;
-
-        let test = "MMMSXXMASM\nMSAMXMSMSA\nAMXSXMAAMM\nMSAMASMSMX\nXMASAMXAMM\nXXAMMXXAMA\nSMSMSASXSS\nSAXAMASAAA\nMAMMMXMMMM\nMXMXAXMASX";
-
-        for c in test.chars() {
+        for c in self.0.chars() {
             if c == '\n' {
                 word_search.push(vec![]);
                 row += 1;
@@ -44,15 +12,59 @@ impl super::lib::Puzzle<usize> for Solver {
                 word_search[row].push(c);
             }
         }
+        word_search
+    }
+}
+
+impl super::lib::Puzzle<usize> for Solver {
+    async fn part_one(&self) -> usize {
+        let word_search = self.parse_input();
 
         let mut total = 0;
-
-        for i in 0..word_search.len() - 4 {
-            for j in 0..word_search[i].len() - 4 {
-                let rows = &word_search[i..i + 4];
-                let block: Vec<&[char]> = rows.iter().map(|v| &v[j..j + 4]).collect();
-                let ranges = scan_block(block);
-                total += ranges.len();
+        for i in 0..word_search.len() - 3 {
+            for j in 0..word_search[i].len() - 3 {
+                let mut substrings: Vec<String> = vec![];
+                // The top row of the scan
+                substrings.push(word_search[i][j..j + 4].iter().collect());
+                // If we're at the bottom, we add the last 4 rows.
+                if i == word_search.len() - 4 {
+                    for k in i + 1..i + 4 {
+                        substrings.push(word_search[k][j..j + 4].iter().collect());
+                    }
+                }
+                // The left-most column of the scan
+                substrings.push(word_search[i..i + 4].iter().map(|v| v[j]).collect());
+                // If we're at the right-most edge, we add the last 4 columns
+                if j == word_search[i].len() - 4 {
+                    for k in j + 1..j + 4 {
+                        substrings.push(word_search[i..i + 4].iter().map(|v| v[k]).collect());
+                    }
+                }
+                // Adding the diagonals
+                let (mut d1, mut d2) = (j, j + 3);
+                let diag_one: String = word_search[i..i + 4]
+                    .iter()
+                    .map(|e| {
+                        let res = e[d1];
+                        d1 += 1;
+                        res
+                    })
+                    .collect();
+                let diag_two: String = word_search[i..i + 4]
+                    .iter()
+                    .map(|e| {
+                        let res = e[d2];
+                        if d2 != 0 {
+                            d2 -= 1;
+                        }
+                        res
+                    })
+                    .collect();
+                substrings.extend(vec![diag_one, diag_two]);
+                total += substrings
+                    .iter()
+                    .filter(|&s| s == "XMAS" || s == "SAMX")
+                    .count();
             }
         }
 
@@ -60,6 +72,43 @@ impl super::lib::Puzzle<usize> for Solver {
     }
 
     async fn part_two(&self) -> usize {
-        0
+        let word_search = self.parse_input();
+
+        let mut total = 0;
+        for i in 0..word_search.len() - 2 {
+            for j in 0..word_search[i].len() - 2 {
+                let mut substrings: Vec<String> = vec![];
+                // The X's in X-MAS
+                let (mut d1, mut d2) = (j, j + 2);
+                let diag_one: String = word_search[i..i + 3]
+                    .iter()
+                    .map(|e| {
+                        let res = e[d1];
+                        d1 += 1;
+                        res
+                    })
+                    .collect();
+                let diag_two: String = word_search[i..i + 3]
+                    .iter()
+                    .map(|e| {
+                        let res = e[d2];
+                        if d2 != 0 {
+                            d2 -= 1;
+                        }
+                        res
+                    })
+                    .collect();
+                substrings.extend(vec![diag_one, diag_two]);
+                let x_mas = substrings
+                    .iter()
+                    .filter(|&s| s == "MAS" || s == "SAM")
+                    .count();
+                if x_mas == 2 {
+                    total += 1
+                }
+            }
+        }
+
+        total
     }
 }
