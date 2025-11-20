af8f8ef9e08a15405b690c1533b44cfe56775920
Eric Hamilton <ericjean873@gmail.com>
2025-11-20 09:35:17 -0800

    day 4 partial (ik what's wrong, multi-counting strings)

--- 2024/src/solutions/day4.rs
+++ 2024/src/solutions/day4.rs
@@ -1,38 +1,42 @@
 pub struct Solver(pub String);
 
-fn scan_block(block: Vec<&[char]>) -> i32 {
+type Point = (usize, usize);
+
+fn scan_block(block: Vec<&[char]>) -> Vec<(Point, Point)> {
     for r in block.as_slice() {
         println!("{:?}", r);
     }
     println!();
-    let mut substrings: Vec<String> = vec![];
+    let mut substrings: Vec<(Point, Point, String)> = vec![];
 
     let diag_one = [block[0][0], block[1][1], block[2][2], block[3][3]];
     let diag_two = [block[0][3], block[1][2], block[2][1], block[3][0]];
-    substrings.push(diag_one.iter().collect());
-    substrings.push(diag_two.iter().collect());
+    substrings.push(((0, 0), (3, 3), diag_one.iter().collect()));
+    substrings.push(((0, 3), (3, 0), diag_two.iter().collect()));
 
     for i in 0..block.len() {
-        substrings.push(block[i].iter().collect());
+        substrings.push(((i, 0), (i, 3), block[i].iter().collect()));
         let col = [block[0][i], block[1][i], block[2][i], block[3][i]];
-        substrings.push(col.iter().collect());
+        substrings.push(((0, i), (3, i), col.iter().collect()));
     }
-    let mut count = 0;
-    for sub in substrings {
-        count += match sub.as_str() {
-            "XMAS" | "SAMX" => 1,
-            _ => 0,
+    let mut ranges = vec![];
+    for sub in substrings.clone() {
+        match sub.2.as_str() {
+            "XMAS" | "SAMX" => ranges.push((sub.0, sub.1)),
+            _ => (),
         }
     }
-    return count;
+    return ranges;
 }
 
-impl super::lib::Puzzle<i32> for Solver {
-    async fn part_one(&self) -> i32 {
+impl super::lib::Puzzle<usize> for Solver {
+    async fn part_one(&self) -> usize {
         let mut word_search = vec![vec![]];
         let mut row = 0;
 
-        for c in self.0.chars() {
+        let test = "MMMSXXMASM\nMSAMXMSMSA\nAMXSXMAAMM\nMSAMASMSMX\nXMASAMXAMM\nXXAMMXXAMA\nSMSMSASXSS\nSAXAMASAAA\nMAMMMXMMMM\nMXMXAXMASX";
+
+        for c in test.chars() {
             if c == '\n' {
                 word_search.push(vec![]);
                 row += 1;
@@ -47,14 +51,15 @@ impl super::lib::Puzzle<i32> for Solver {
             for j in 0..word_search[i].len() - 4 {
                 let rows = &word_search[i..i + 4];
                 let block: Vec<&[char]> = rows.iter().map(|v| &v[j..j + 4]).collect();
-                total += scan_block(block);
+                let ranges = scan_block(block);
+                total += ranges.len();
             }
         }
 
         total
     }
 
-    async fn part_two(&self) -> i32 {
+    async fn part_two(&self) -> usize {
         0
     }
 }
