d6ea0d7a6d27502b6d522eb3b2da8ee7b583970c
Eric Hamilton <ericjean873@gmail.com>
2025-11-21 12:31:54 -0800

    made d6p2 ~4x faster (still pretty slow)

--- 2024/src/solutions/day6.rs
+++ 2024/src/solutions/day6.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 pub struct Solver(pub String);
 
@@ -9,6 +9,7 @@ impl Solver {
         let mut map = Map::new();
         let mut init = (0, 0);
         let mut r = 0;
+        let _test = "....#.....\n.........#\n..........\n..#.......\n.......#..\n..........\n.#..^.....\n........#.\n#.........\n......#...";
         for line in self.0.split("\n") {
             map.push(line.chars().collect());
             match map[r].iter().position(|&c| c == '^') {
@@ -56,40 +57,61 @@ fn step(map: &Map, pos: &Coord, dir: &Direction) -> Option<(Coord, Direction)> {
     }
 }
 
+/// Gets the number of distinct positions in the map; or returns None if it's a cycle.
+fn distinct_pos(
+    map: &Map,
+    init_pos: &Coord,
+    init_dir: &Direction,
+) -> Option<HashMap<Coord, HashSet<Direction>>> {
+    let (mut curr_pos, mut curr_dir) = (*init_pos, *init_dir);
+    let mut visited = HashMap::new();
+    visited.insert(*init_pos, HashSet::from([*init_dir]));
+    while let Some((next_pos, next_dir)) = step(map, &curr_pos, &curr_dir) {
+        (curr_pos, curr_dir) = (next_pos, next_dir);
+        // if we try to insert the current direction for this position, and it's already there,
+        // it's a cycle, so we return None.
+        if let Some(set) = visited.get_mut(&curr_pos) {
+            if !set.insert(curr_dir) {
+                return None;
+            }
+        } else {
+            visited.insert(curr_pos, HashSet::from([curr_dir]));
+        }
+    }
+    Some(visited)
+}
+
 impl super::lib::Puzzle<usize> for Solver {
     async fn part_one(&self) -> usize {
-        let (map, init) = self.parse_input();
-        let mut distinct_set: HashSet<Coord> = HashSet::from([init]);
-        let mut curr_pos = init;
-        let mut curr_dir = Direction::N;
-        while let Some((pos, dir)) = step(&map, &curr_pos, &curr_dir) {
-            (curr_pos, curr_dir) = (pos, dir);
-            distinct_set.insert(curr_pos);
-        }
-        distinct_set.len()
+        let (map, init_pos) = self.parse_input();
+        let distinct = distinct_pos(&map, &init_pos, &Direction::N).unwrap();
+        return distinct.len();
     }
 
     // this is bad and runs super slow. ill fix it prommy :3
     async fn part_two(&self) -> usize {
-        let (map, init) = self.parse_input();
-        let mut loop_obstacle_count = 0;
-        for i in 0..map.len() {
-            for j in 0..map[i].len() {
-                let mut map = map.clone();
-                map[i][j] = '#';
-                let mut loop_set: HashSet<(Coord, Direction)> = HashSet::new();
-                let mut curr_pos = init;
-                let mut curr_dir = Direction::N;
-                while let Some(next) = step(&map, &curr_pos, &curr_dir) {
-                    (curr_pos, curr_dir) = next;
-                    if loop_set.contains(&next) {
-                        loop_obstacle_count += 1;
-                        break;
-                    }
-                    loop_set.insert(next);
+        let (map, init_pos) = self.parse_input();
+        let visited = distinct_pos(&map, &init_pos, &Direction::N).unwrap();
+        let mut v_iter = visited.iter();
+        let mut obstacle_set = HashSet::new();
+        while let Some((pos, dirs)) = v_iter.next() {
+            for dir in dirs {
+                let obstacle_pos = match step(&map, pos, dir) {
+                    Some((p, _)) => p,
+                    None => continue,
+                };
+                let mut obstacle_map = map.clone();
+                let map_loc = &mut obstacle_map[obstacle_pos.0][obstacle_pos.1];
+                *map_loc = match *map_loc != '#' {
+                    true => '#',
+                    false => continue,
+                };
+                // This isn't great, but it runs okay-ish (about 20s)
+                if distinct_pos(&obstacle_map, &init_pos, &Direction::N).is_none() {
+                    obstacle_set.insert(obstacle_pos);
                 }
             }
         }
-        loop_obstacle_count
+        obstacle_set.len()
     }
 }
