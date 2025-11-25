ea85479bd3332dd7c4c1cb1d243b05336363b727
Eric Hamilton <ericjean873@gmail.com>
2025-11-25 14:38:42 -0800

    day 11 part one completed synchronous

--- 2024/src/solutions/day11.rs
+++ 2024/src/solutions/day11.rs
@@ -1,5 +1,4 @@
-use std::{fmt::Display, num::ParseIntError, str::FromStr, sync::Arc, time::Instant};
-use tokio::task::JoinSet;
+use std::{fmt::Display, num::ParseIntError, str::FromStr, time::Instant};
 
 pub struct Stones(Vec<u64>);
 
@@ -26,35 +25,20 @@ impl Display for Stones {
 }
 
 impl Stones {
-    pub async fn blink(&self) -> Self {
-        fn task(chunk: &[u64]) -> Vec<u64> {
-            let mut new_stones = vec![];
-            for stone in chunk.get(0) {
-                if *stone == 0 {
-                    new_stones.push(1);
-                } else if (stone.ilog10() + 1) % 2 == 0 {
-                    let base = (10 as u64).pow((stone.ilog10() + 1) / 2);
-                    new_stones.push(*stone / base);
-                    new_stones.push(*stone % base);
-                } else {
-                    new_stones.push(*stone * 2024);
-                }
+    pub fn blink(&mut self) {
+        let mut new_stones = vec![];
+        for stone in self.0.iter_mut() {
+            if *stone == 0 {
+                *stone = 1;
+            } else if (stone.ilog10() + 1) % 2 == 0 {
+                let base = (10 as u64).pow((stone.ilog10() + 1) / 2);
+                new_stones.push(*stone / base);
+                *stone %= base;
+            } else {
+                *stone *= 2024;
             }
-            new_stones
         }
-
-        let mut new_vec = vec![];
-        let mut task_set = JoinSet::new();
-        let stones_arc = Arc::new(self).clone();
-        let chunk_size = self.len() / 4;
-        for chunk in stones_arc.0.chunks(chunk_size) {
-            task_set.spawn_blocking(move || task(chunk));
-        }
-        let join_results = task_set.join_all().await;
-        for result in join_results {
-            new_vec.extend(result);
-        }
-        Self(new_vec)
+        self.0.extend(new_stones)
     }
 
     pub fn len(&self) -> usize {
@@ -68,7 +52,7 @@ impl super::lib::Puzzle<usize> for Solver {
     async fn part_one(&self) -> usize {
         let mut stones = Stones::from_str(self.0.as_str()).unwrap();
         for _ in 0..25 {
-            stones = stones.blink().await;
+            stones.blink();
         }
         stones.len()
     }
@@ -77,7 +61,7 @@ impl super::lib::Puzzle<usize> for Solver {
         let mut stones = Stones::from_str(self.0.as_str()).unwrap();
         for i in 0..0 {
             let blink_instant = Instant::now();
-            stones.blink().await;
+            stones.blink();
             println!("Blink {}: {}ms", i, blink_instant.elapsed().as_millis());
         }
         stones.len()
