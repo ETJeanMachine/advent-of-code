497366cf65c31a7df1c94343d6fdc0eb746647f4
Eric Hamilton <ericjean873@gmail.com>
2025-11-25 15:41:30 -0800

    still no good; program crashes wsl :p

--- 2024/src/solutions/day11.rs
+++ 2024/src/solutions/day11.rs
@@ -1,4 +1,13 @@
-use std::{fmt::Display, num::ParseIntError, str::FromStr, time::Instant};
+use std::{
+    fmt::Display,
+    num::ParseIntError,
+    str::FromStr,
+    sync::{Arc, Mutex},
+    time::Instant,
+};
+
+use rayon::{iter::ParallelIterator, slice::ParallelSliceMut};
+use tokio::task::JoinSet;
 
 pub struct Stones(Vec<u64>);
 
@@ -41,6 +50,32 @@ impl Stones {
         self.0.extend(new_stones)
     }
 
+    pub fn blink_parallel(&mut self) {
+        let n = if self.len() > 202019 { 16 } else { 4 };
+        let chunk_size = self.len() / n;
+        let new_stones_vec: Vec<_> = self
+            .0
+            .par_chunks_mut(chunk_size)
+            .map(move |chunk| {
+                chunk.iter_mut().fold(vec![], move |mut new_stones, stone| {
+                    if *stone == 0 {
+                        *stone = 1;
+                    } else if (stone.ilog10() + 1) % 2 == 0 {
+                        let base = (10 as u64).pow((stone.ilog10() + 1) / 2);
+                        new_stones.push(*stone / base);
+                        *stone %= base;
+                    } else {
+                        *stone *= 2024;
+                    }
+                    new_stones
+                })
+            })
+            .collect();
+        for new_stones in new_stones_vec {
+            self.0.extend(new_stones);
+        }
+    }
+
     pub fn len(&self) -> usize {
         self.0.len()
     }
@@ -59,10 +94,11 @@ impl super::lib::Puzzle<usize> for Solver {
 
     async fn part_two(&self) -> usize {
         let mut stones = Stones::from_str(self.0.as_str()).unwrap();
-        for i in 0..0 {
+        for i in 0..75 {
             let blink_instant = Instant::now();
-            stones.blink();
+            stones.blink_parallel();
             println!("Blink {}: {}ms", i, blink_instant.elapsed().as_millis());
+            println!("Blink {}: {}", i, stones.len());
         }
         stones.len()
     }
