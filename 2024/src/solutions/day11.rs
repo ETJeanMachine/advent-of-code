a35ff263ec7d8bb1ed2e2291f5faf29815f3e5ee
Eric Hamilton <ericjean873@gmail.com>
2025-11-25 17:00:43 -0800

    rewrote using memoization ( i forgor this was a thing u could do :O )

--- 2024/src/solutions/day11.rs
+++ 2024/src/solutions/day11.rs
@@ -1,105 +1,53 @@
-use std::{
-    fmt::Display,
-    num::ParseIntError,
-    str::FromStr,
-    sync::{Arc, Mutex},
-    time::Instant,
-};
-
-use rayon::{iter::ParallelIterator, slice::ParallelSliceMut};
-use tokio::task::JoinSet;
-
-pub struct Stones(Vec<u64>);
-
-impl FromStr for Stones {
-    type Err = ParseIntError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parsed = s
-            .split_whitespace()
-            .map_while(|substr| substr.parse().ok())
-            .collect();
-        Ok(Self(parsed))
-    }
-}
-
-impl Display for Stones {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let fmt_str = self
-            .0
-            .iter()
-            .fold(String::new(), |acc, x| format!("{} {}", acc, x));
-        write!(f, "{}", fmt_str.trim())
-    }
-}
-
-impl Stones {
-    pub fn blink(&mut self) {
-        let mut new_stones = vec![];
-        for stone in self.0.iter_mut() {
-            if *stone == 0 {
-                *stone = 1;
-            } else if (stone.ilog10() + 1) % 2 == 0 {
-                let base = (10 as u64).pow((stone.ilog10() + 1) / 2);
-                new_stones.push(*stone / base);
-                *stone %= base;
-            } else {
-                *stone *= 2024;
-            }
-        }
-        self.0.extend(new_stones)
-    }
-
-    pub fn blink_parallel(&mut self) {
-        let n = if self.len() > 202019 { 16 } else { 4 };
-        let chunk_size = self.len() / n;
-        let new_stones_vec: Vec<_> = self
-            .0
-            .par_chunks_mut(chunk_size)
-            .map(move |chunk| {
-                chunk.iter_mut().fold(vec![], move |mut new_stones, stone| {
-                    if *stone == 0 {
-                        *stone = 1;
-                    } else if (stone.ilog10() + 1) % 2 == 0 {
-                        let base = (10 as u64).pow((stone.ilog10() + 1) / 2);
-                        new_stones.push(*stone / base);
-                        *stone %= base;
-                    } else {
-                        *stone *= 2024;
-                    }
-                    new_stones
-                })
-            })
-            .collect();
-        for new_stones in new_stones_vec {
-            self.0.extend(new_stones);
-        }
-    }
-
-    pub fn len(&self) -> usize {
-        self.0.len()
-    }
-}
+use std::collections::HashMap;
 
 pub struct Solver(pub String);
 
-impl super::lib::Puzzle<usize> for Solver {
-    async fn part_one(&self) -> usize {
-        let mut stones = Stones::from_str(self.0.as_str()).unwrap();
-        for _ in 0..25 {
-            stones.blink();
-        }
-        stones.len()
+impl Solver {
+    fn parse_input(&self) -> Vec<u64> {
+        self.0
+            .split_whitespace()
+            .map(|substr| substr.parse().unwrap())
+            .collect()
+    }
+}
+
+fn blink(stone: &u64, memoized_stones: &mut Vec<HashMap<u64, u64>>, blinks: &usize) -> u64 {
+    if *blinks == 0 {
+        return 1;
+    } else if let Some(count) = memoized_stones[*blinks - 1].get(stone) {
+        return *count;
+    }
+    let mut new_stones = vec![];
+    if *stone == 0 {
+        new_stones.push(1);
+    } else if (stone.ilog10() + 1) % 2 == 0 {
+        let base = (10 as u64).pow((stone.ilog10() + 1) / 2);
+        new_stones.push(*stone / base);
+        new_stones.push(*stone % base);
+    } else {
+        new_stones.push(*stone * 2024);
+    }
+    let result = new_stones.iter().fold(0, |acc, new_stone| {
+        acc + blink(new_stone, memoized_stones, &(blinks - 1))
+    });
+    memoized_stones[*blinks - 1].insert(*stone, result);
+    result
+}
+
+impl super::lib::Puzzle<u64> for Solver {
+    async fn part_one(&self) -> u64 {
+        let stones = self.parse_input();
+        let mut memoized_stones = (0..25).map(|_| HashMap::new()).collect();
+        stones.iter().fold(0, |acc, stone| {
+            acc + blink(stone, &mut memoized_stones, &25)
+        })
     }
 
-    async fn part_two(&self) -> usize {
-        let mut stones = Stones::from_str(self.0.as_str()).unwrap();
-        for i in 0..75 {
-            let blink_instant = Instant::now();
-            stones.blink_parallel();
-            println!("Blink {}: {}ms", i, blink_instant.elapsed().as_millis());
-            println!("Blink {}: {}", i, stones.len());
-        }
-        stones.len()
+    async fn part_two(&self) -> u64 {
+        let stones = self.parse_input();
+        let mut memoized_stones = (0..75).map(|_| HashMap::new()).collect();
+        stones.iter().fold(0, |acc, stone| {
+            acc + blink(stone, &mut memoized_stones, &75)
+        })
     }
 }
