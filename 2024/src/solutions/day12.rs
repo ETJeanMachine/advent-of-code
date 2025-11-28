5ff29fa23c4605639c5c50a232571da0951169f1
Eric Hamilton <ericjean873@gmail.com>
2025-11-27 23:19:10 -0800

    day 12 pt2 partial (does not work yet)

--- 2024/src/solutions/day12.rs
+++ 2024/src/solutions/day12.rs
@@ -22,22 +22,34 @@ impl FromStr for Farm {
     }
 }
 
+#[derive(PartialEq, Eq, Clone, Copy, Hash)]
+enum Direction {
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST,
+}
+
 impl Farm {
-    fn neighbouring_plants(&self, plant: char, pos: (usize, usize)) -> Vec<(usize, usize)> {
+    fn neighbouring_plants(
+        &self,
+        plant: char,
+        pos: (usize, usize),
+    ) -> Vec<((usize, usize), Direction)> {
         let mut neighbours = vec![];
         let farm_map = &self.farm_map;
         let (row, col) = pos;
         if row > 0 && farm_map[row - 1][col] == plant {
-            neighbours.push((row - 1, col));
+            neighbours.push(((row - 1, col), Direction::NORTH));
         }
         if row < self.height - 1 && farm_map[row + 1][col] == plant {
-            neighbours.push((row + 1, col));
+            neighbours.push(((row + 1, col), Direction::SOUTH));
         }
         if col > 0 && farm_map[row][col - 1] == plant {
-            neighbours.push((row, col - 1));
+            neighbours.push(((row, col - 1), Direction::WEST));
         }
         if col < self.width - 1 && farm_map[row][col + 1] == plant {
-            neighbours.push((row, col + 1));
+            neighbours.push(((row, col + 1), Direction::EAST));
         }
         neighbours
     }
@@ -66,7 +78,7 @@ impl Farm {
         let plant = self.farm_map[curr.0][curr.1];
         let neighbours = self.neighbouring_plants(plant, curr);
         let (mut perimeter, mut area) = (4 - neighbours.len() as u32, 1);
-        for neighbour in neighbours {
+        for (neighbour, _) in neighbours {
             if !discovered.contains(&neighbour) {
                 let (next_perimeter, next_area) =
                     self.region_area_and_perimeter(discovered, neighbour);
@@ -77,7 +89,7 @@ impl Farm {
         (perimeter, area)
     }
 
-    pub fn total_region_cost(&self) -> u32 {
+    pub fn total_region_cost_by_perimeter(&self) -> u32 {
         let mut total_cost = 0;
         let mut discovered = HashSet::new();
         let mut curr_row = 0;
@@ -88,6 +100,56 @@ impl Farm {
         }
         total_cost
     }
+
+    fn region_area_and_sides(
+        &self,
+        discovered: &mut HashSet<(usize, usize)>,
+        sides: &mut HashSet<(usize, Direction)>,
+        curr: (usize, usize),
+    ) -> u32 {
+        discovered.insert(curr);
+        let (row, col) = curr;
+        let plant = self.farm_map[row][col];
+        let neighbours = self.neighbouring_plants(plant, curr);
+        let mut area = 1;
+        let non_side_dirs: Vec<_> = neighbours.iter().map(|(_, dir)| *dir).collect();
+        let side_dirs: Vec<_> = vec![
+            Direction::NORTH,
+            Direction::SOUTH,
+            Direction::EAST,
+            Direction::WEST,
+        ]
+        .iter()
+        .filter(|dir| !non_side_dirs.contains(dir))
+        .map(|dir| *dir)
+        .collect();
+        for dir in side_dirs {
+            sides.insert(match dir {
+                Direction::NORTH | Direction::SOUTH => (row, dir),
+                Direction::EAST | Direction::WEST => (col, dir),
+            });
+        }
+        for (neighbour, _) in neighbours {
+            if !discovered.contains(&neighbour) {
+                let next_area = self.region_area_and_sides(discovered, sides, neighbour);
+                area += next_area;
+            }
+        }
+        area
+    }
+
+    pub fn total_region_cost_by_sides(&self) -> u32 {
+        let mut total_cost = 0;
+        let mut discovered = HashSet::new();
+        let mut curr_row = 0;
+        while let Some(curr) = self.next_undiscovered(curr_row, &discovered) {
+            curr_row = curr.0;
+            let mut sides = HashSet::new();
+            let area = self.region_area_and_sides(&mut discovered, &mut sides, curr);
+            total_cost += sides.len() as u32 * area
+        }
+        total_cost
+    }
 }
 
 pub struct Solver(pub String);
@@ -95,10 +157,11 @@ pub struct Solver(pub String);
 impl super::lib::Puzzle<u32> for Solver {
     async fn part_one(&self) -> u32 {
         let farm = Farm::from_str(self.0.as_str()).unwrap();
-        farm.total_region_cost()
+        farm.total_region_cost_by_perimeter()
     }
 
     async fn part_two(&self) -> u32 {
-        0
+        let farm = Farm::from_str(self.0.as_str()).unwrap();
+        farm.total_region_cost_by_sides()
     }
 }
