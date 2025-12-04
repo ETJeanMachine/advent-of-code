6e73c037f2dfabb8ea70b867a660228481775933
Eric Hamilton <ericjean873@gmail.com>
2025-12-03 21:41:28 -0800

    re-added template

---
+++ 2025/solutions/.template/template.go
@@ -0,0 +1,13 @@
+package template
+
+func partOne(input string) string {
+	return ""
+}
+
+func partTwo(input string) string {
+	return ""
+}
+
+func Puzzles() (func(string) string, func(string) string) {
+	return partOne, partTwo
+}
