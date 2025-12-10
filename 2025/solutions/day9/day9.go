519511cd772a72e60e388d9e1e46265800f051b4
Eric Hamilton <ericjean873@gmail.com>
2025-12-09 17:42:08 -0800

    day 9 bump

--- 2025/solutions/day9/day9.go
+++ 2025/solutions/day9/day9.go
@@ -7,6 +7,15 @@ import (
 
 type Point struct{ x, y int }
 
+type Line struct{ p1, p2 Point }
+
+func NewLine(p1 Point, p2 Point) Line {
+	if p1.x < p2.x || p1.y < p2.y {
+		return Line{p1, p2}
+	}
+	return Line{p2, p1}
+}
+
 func (p Point) Area(o Point) int {
 	width := p.x - o.x
 	if width < 0 {
@@ -45,8 +54,34 @@ func partOne(input string) string {
 	return strconv.Itoa(max_area)
 }
 
+func inBounds(lines []Line, p Point, o Point) bool {
+	// alt_p, alt_o := Point{p.x, o.y}, Point{o.x, p.y}
+	isInBounds := true
+	for _, line := range lines {
+		if line.p1.x == line.p2.x {
+		} else {
+		}
+	}
+	return isInBounds
+}
+
 func partTwo(input string) string {
-	return "0"
+	// input = "7,1\n11,1\n11,7\n9,7\n9,5\n2,5\n2,3\n7,3"
+	points := parseInput(input)
+	lines := []Line{NewLine(points[0], points[len(points)-1])}
+	for i := 0; i < len(points)-1; i += 2 {
+		lines = append(lines, NewLine(points[i], points[i+1]))
+	}
+	var max_area = 0
+	for idx, p := range points[:len(points)-1] {
+		for _, o := range points[idx+1:] {
+			area := p.Area(o)
+			if area > max_area && inBounds(lines, p, o) {
+				max_area = area
+			}
+		}
+	}
+	return strconv.Itoa(max_area)
 }
 
 func Puzzles() (func(string) string, func(string) string) {
