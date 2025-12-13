7e0e97cfb8200e69618ea4648abc075a2eb0e4c4
Eric Hamilton <ericjean873@gmail.com>
2025-12-12 17:07:56 -0800

    bump

--- 2025/solutions/day9/day9.go
+++ 2025/solutions/day9/day9.go
@@ -7,12 +7,6 @@ import (
 
 type Point struct{ x, y int }
 
-type Line struct{ p1, p2 Point }
-
-func NewLine(p1 Point, p2 Point) Line {
-	return Line{p1, p2}
-}
-
 type Rectangle struct {
 	topLeft, botRight Point
 }
@@ -31,7 +25,14 @@ func NewRectangle(p1 Point, p2 Point) Rectangle {
 	return Rectangle{topLeft, botRight}
 }
 
-func (r Rectangle) InRectangle(p Point) bool {
+// Returns the points for the rectangle, ordered top-bottom, left-right.
+func (r Rectangle) Points() [4]Point {
+	botLeft := Point{r.topLeft.x, r.botRight.y}
+	topRight := Point{r.botRight.x, r.topLeft.y}
+	return [4]Point{r.topLeft, topRight, botLeft, r.botRight}
+}
+
+func (r Rectangle) ContainsPoint(p Point) bool {
 	fitsWidth := r.topLeft.x <= p.x && p.x <= r.botRight.x
 	fitsHeight := r.topLeft.y <= p.y && p.y <= r.botRight.y
 	return fitsHeight && fitsWidth
@@ -45,6 +46,26 @@ func (r Rectangle) Area() int {
 
 type Shape struct{ rectangles []Rectangle }
 
+func NewShape(points []Point) Shape {
+	type vertline struct{ topY, botY int }
+
+	return Shape{}
+}
+
+func (s Shape) ContainsRectangle(rect Rectangle) bool {
+	points := rect.Points()
+	for _, r := range s.rectangles {
+		containsAll := true
+		for _, p := range points {
+			containsAll = containsAll && r.ContainsPoint(p)
+		}
+		if containsAll {
+			return true
+		}
+	}
+	return false
+}
+
 func (p Point) Area(o Point) int {
 	width := p.x - o.x
 	if width < 0 {
@@ -56,21 +77,6 @@ func (p Point) Area(o Point) int {
 	}
 	area := (width + 1) * (height + 1)
 	return area
-}
-
-func inBounds(lines []Line, p Point, o Point) bool {
-	alts := []Point{Point{p.x, o.y}, Point{o.x, p.y}}
-	fits := false
-	for i := 0; i < len(lines)-1; i += 2 {
-		line1, line2 := lines[i], lines[i+1]
-		rect := NewRectangle(line1.p1, line2.p2)
-		isInBounds := true
-		for _, alt := range alts {
-			isInBounds = isInBounds && rect.InRectangle(alt)
-		}
-		fits = fits != isInBounds
-	}
-	return fits
 }
 
 func parseInput(input string) []Point {
@@ -103,16 +109,13 @@ func partOne(input string) string {
 func partTwo(input string) string {
 	input = "7,1\n11,1\n11,7\n9,7\n9,5\n2,5\n2,3\n7,3"
 	points := parseInput(input)
-	lines := []Line{}
-	for i := range len(points) - 1 {
-		lines = append(lines, NewLine(points[i], points[i+1]))
-	}
-	lines = append(lines, NewLine(points[0], points[len(points)-1]))
+	shape := NewShape(points)
 	var max_area = 0
 	for idx, p := range points[:len(points)-1] {
 		for _, o := range points[idx+1:] {
-			area := p.Area(o)
-			if area > max_area && inBounds(lines, p, o) {
+			rect := NewRectangle(p, o)
+			area := rect.Area()
+			if area > max_area && shape.ContainsRectangle(rect) {
 				max_area = area
 			}
 		}
